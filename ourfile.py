e6ef1b1a8ff697a40d3c16eb0467dd5f6a532229
catherine-orr <143897544+catherine-orr@users.noreply.github.com>
2024-01-27 19:43:37 -0500

    added some code to ourfile

--- ourfile.py
+++ ourfile.py
@@ -1,1 +1,4 @@
-print("hello")
+hair_color = input("what is your hair color?")
+eye_color = input("what color are your eyes")
+height = input("how tall are you?")
+ethnicity = input("what is your ethnicity?")
