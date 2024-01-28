41e15c32c1e50dbbdb7eb9459b082161666f4ba6
catherine-orr <143897544+catherine-orr@users.noreply.github.com>
2024-01-27 19:56:26 -0500

    added the starting Flask stuff

--- ourfile.py
+++ ourfile.py
@@ -1,3 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 hair_color = input("what is your hair color?")
 eye_color = input("what color are your eyes")
 height = input("how tall are you?")
